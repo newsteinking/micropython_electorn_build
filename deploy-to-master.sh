09c8402e8840be0afaa2266019b2cd2d327b1f4f
newstein33 <newstein33@gmail.com>
2019-03-27 11:35:06 +0900

    dd

--- deploy-to-master.sh
+++ deploy-to-master.sh
@@ -12,43 +12,43 @@ git config --global user.name  "newsteinking"
 # build (CHANGE THIS)
 mkdir editor
 cd editor
-mkdir scratch-gui
-git clone https://github.com/LLK/scratch-gui.git scratch-gui-source
-cd scratch-gui-source
+mkdir micropython-gui
+git clone https://github.com/LLK/micropython.git micropython-gui-source
+cd micropython-gui-source
 npm install > /dev/null 2>&1
 npm run build > /dev/null 2>&1
 cp -r ./build/* ../
 cd ../
-rm -r -f "scratch-gui-source"
+rm -r -f "micropython-gui-source"
 
 cd ../
 echo "Win x64"
-./node_modules/.bin/electron-packager . "Scratch 3.0" --asar --platform win32 --arch x64 --out out --icon icon.ico --prune false
+./node_modules/.bin/electron-packager . "MicroPython1.0" --asar --platform win32 --arch x64 --out out --icon icon.ico --prune false
 echo "Win x32"
-./node_modules/.bin/electron-packager . "Scratch 3.0" --asar --platform win32 --arch ia32 --out out --icon icon.ico --prune false
+./node_modules/.bin/electron-packager . "MicroPython1.0" --asar --platform win32 --arch ia32 --out out --icon icon.ico --prune false
 echo "Linux x64"
-./node_modules/.bin/electron-packager . "Scratch 3.0" --asar --platform linux --arch x64 --out out --icon icon.png --prune false
+./node_modules/.bin/electron-packager . "MicroPython1.0" --asar --platform linux --arch x64 --out out --icon icon.png --prune false
 echo "Linux x32"
-./node_modules/.bin/electron-packager . "Scratch 3.0" --asar --platform linux --arch ia32 --out out --icon icon.png --prune false
+./node_modules/.bin/electron-packager . "MicroPython1.0" --asar --platform linux --arch ia32 --out out --icon icon.png --prune false
 echo "Linux ARM"
-./node_modules/.bin/electron-packager . "Scratch 3.0" --asar --platform linux --arch armv7l --out out --icon icon.png --prune false
+./node_modules/.bin/electron-packager . "MicroPython1.0" --asar --platform linux --arch armv7l --out out --icon icon.png --prune false
 echo "Mac x64"
-./node_modules/.bin/electron-packager . "Scratch 3.0" --asar --platform darwin --arch x64 --out out --icon icon.icns --prune false
+./node_modules/.bin/electron-packager . "MicroPython1.0" --asar --platform darwin --arch x64 --out out --icon icon.icns --prune false
 
 # deploy
 cd out
-zip -r -q -9 Win32-x64.zip "Scratch 3.0-win32-x64"
-rm -r "Scratch 3.0-win32-x64"
-zip -r -q -9 Win32-ia32.zip "Scratch 3.0-win32-ia32"
-rm -r "Scratch 3.0-win32-ia32"
-tar -cvzf Linux-x64.tar.gz "Scratch 3.0-linux-x64" > /dev/null 2>&1
-rm -r "Scratch 3.0-linux-x64"
-tar -cvzf Linux-ia32.tar.gz "Scratch 3.0-linux-ia32" > /dev/null 2>&1
-rm -r "Scratch 3.0-linux-ia32"
-tar -cvzf Linux-armv7l.tar.gz "Scratch 3.0-linux-armv7l" > /dev/null 2>&1
-rm -r "Scratch 3.0-linux-armv7l"
-tar -cvzf Mac-x64.tar.gz "Scratch 3.0-darwin-x64" > /dev/null 2>&1
-rm -r "Scratch 3.0-darwin-x64"
+zip -r -q -9 MicroPython1.0_Win32-x64.zip "MicroPython1.0-win32-x64"
+rm -r "MicroPython1.0-win32-x64"
+zip -r -q -9 MicroPython1.0_Win32-ia32.zip "MicroPython1.0-win32-ia32"
+rm -r "MicroPython1.0-win32-ia32"
+tar -cvzf MicroPython1.0_Linux-x64.tar.gz "MicroPython1.0-linux-x64" > /dev/null 2>&1
+rm -r "MicroPython1.0linux-x64"
+tar -cvzf MicroPython1.0_Linux-ia32.tar.gz "MicroPython1.0-linux-ia32" > /dev/null 2>&1
+rm -r "MicroPython1.0-linux-ia32"
+tar -cvzf MicroPython1.0_Linux-armv7l.tar.gz "MicroPython1.0-linux-armv7l" > /dev/null 2>&1
+rm -r "MicroPython1.0-linux-armv7l"
+tar -cvzf MicroPython1.0_Mac-x64.tar.gz "MicroPython1.0-darwin-x64" > /dev/null 2>&1
+rm -r "MicroPython1.0-darwin-x64"
 cp ../index.md ./
 cp ../_config.yml ./
 git init
